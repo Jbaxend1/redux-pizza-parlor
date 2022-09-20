914dd727d751a0c85ad410b10e72d7b1e8dc1c62
Latisha Copeland <lcopeland001@gmail.com>
2022-09-20 16:41:11 -0500

    create reducers for name, street, city, zip

--- src/index.js
+++ src/index.js
@@ -2,5 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { combineReducers, applyMiddleware } from 'redux';
+import {Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Reducers
+const name = (state = '', action) => {
+    if (action.type === 'SET_NAME') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_ALL') {
+        return '';
+    }
+    return state;
+}
+
+const street = (state = '', action) => {
+    if (action.type === 'SET_STREET') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_ALL') {
+        return '';
+    }
+    return state;
+}
+
+const city = (state = '', action) => {
+    if (action.type === 'SET_city') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_ALL') {
+        return '';
+    }
+    return state;
+}
+
+const zip = (state = '', action) => {
+    if (action.type === 'SET_zip') {
+        return action.payload;
+    } else if (action.type === 'CLEAR_ALL') {
+        return '';
+    }
+    return state;
+}
+
+// const pickupOrDelivery = (state = true, action) => {
+//     // if (action.type === 'SET_PICKORDELIVERY') {
+//     //     return action.payload;
+//     // } else if (action.type === 'CLEAR_ALL') {
+//     //     return '';
+//     // }
+//     return state;
+// }
+
+
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
