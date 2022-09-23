4949fd1355eb1399877707c1ad007450f309557f
Jonathon Baxendell <j.bax626@gmail.com>
2022-09-22 19:23:45 -0500

    next button added

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -7,16 +7,17 @@ const Info = () => {
         <>
             <h3>Step: 2</h3>
             <h2>Your Information</h2>
+            <button>Next</button>
             <div className="customer">
                 <form>
-                    <input placeholder='Name'></input>
-                    <input placeholder='Street Address'></input>
-                    <input placeholder='City'></input>
-                    <input placeholder='Zip'></input>
+                    <input type='text' placeholder='Name'></input>
+                    <input type='text' placeholder='Street Address'></input>
+                    <input type='text' placeholder='City'></input>
+                    <input type='text' placeholder='Zip'></input>
                 </form>
+            </div>
                 <input type="radio" value="pickup" name="pickup" /> Pickup
                 <input type="radio" value="delivery" name="delivery" /> Delivery
-            </div>
         </>
     )
 
