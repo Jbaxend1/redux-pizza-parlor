7dc4a724d9e2543ecbe236be4069adf377fab915
Jonathon Baxendell <j.bax626@gmail.com>
2022-09-22 20:12:14 -0500

    input onchange handlers and reducers added

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -3,6 +3,37 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 
 const Info = () => {
+
+    // const history = useHistory();
+
+    const dispatch = useDispatch();
+
+    const handleName = (event) => {
+        dispatch({ type: 'SET_CUSTOMER',
+        payload: event.target.value });
+    }
+
+    const handleStreet = (event) => {
+        dispatch({
+            type: 'SET_STREET',
+            payload: event.target.value
+        });
+    }
+
+    const handleCity = (e) => {
+        dispatch({
+            type: 'SET_CITY',
+            payload: e.target.value
+        });
+    }
+
+    const handleZip = (e) => {
+        dispatch({
+            type: 'SET_ZIP',
+            payload: e.target.value
+        });
+    }
+
     return (
         <>
             <h3>Step: 2</h3>
@@ -10,10 +41,10 @@ const Info = () => {
             <button>Next</button>
             <div className="customer">
                 <form>
-                    <input type='text' placeholder='Name'></input>
-                    <input type='text' placeholder='Street Address'></input>
-                    <input type='text' placeholder='City'></input>
-                    <input type='text' placeholder='Zip'></input>
+                    <input onChange={handleName} type='text' placeholder='Name'></input>
+                    <input onChange={handleStreet} type='text' placeholder='Street Address'></input>
+                    <input onChange={handleCity} type='text' placeholder='City'></input>
+                    <input onChange={handleZip} type='text' placeholder='Zip'></input>
                 </form>
             </div>
                 <input type="radio" value="pickup" name="pickup" /> Pickup
